94377f55e66eec8c04f6bafd65740b637def6420
Antonino <antoninoscaffidi@gmail.com>
2019-03-05 14:22:18 +0100

    Added create and update action in pois controller

--- app/controllers/api/v1/pois_controller.rb
+++ app/controllers/api/v1/pois_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::PoisController < ApplicationController
-  before_action :set_poi, only: [:show]
+  before_action :set_poi, only: [:show, :update, :destroy]
   
   def index
-    #@pois = Poi.all.order(:id)
-    #render json: @pois
     pois = Poi.all
     render json: PoiSerializer.new(pois).serialized_json
   end
@@ -11,10 +9,36 @@ class Api::V1::PoisController < ApplicationController
   def show
     render json: @poi
   end
+
+  def create
+    @poi = Poi.new(poi_params)
+    if @poi.save
+      render json: @poi, status: :created 
+    else
+      render json: {errors: @poi.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @poi.update(poi_params)
+      render json: @poi, status: :ok 
+    else
+      render json: {errors: @poi.errors}, status: :unprocessable_entity
+    end
+  end 
+
+  def destroy
+    @poi.destroy
+    head 204
+  end 
+
  
   private 
   def set_poi
     @poi = Poi.find(params[:id])
   end
- 
+  
+  def poi_params
+    params.require(:poi).permit(:name, :description, :latitude, :longitude)
+  end 
 end
